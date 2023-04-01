4ba1c8b02cff8f05ef4ebc89dfa62bc7c564d322
Ruben Brugman <ruben.brugman@spendlab.com>
2023-04-01 14:55:46 +0200

    Tried out some features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'demo-joris';
+
+  helloWorld = 'Hello word';
+  veryLongNumber = 1237439827498732;
+
+  name: string | undefined;
+
+  loadingFruits = true;
+  fruits = ['Apple', 'Pear', 'Cherry'];
+
+  onClickLog(): void {
+    console.log(this.name);
+  }
+
+  onClickSetToRuben(): void {
+    this.name = 'Ruben';
+  }
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
