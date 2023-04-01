9030178005be22143029c50c610e154baa534a69
Ruben Brugman <ruben.brugman@spendlab.com>
2023-04-01 16:36:14 +0200

    Forgot to stage file

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -11,7 +11,7 @@ export class PokemonService {
   constructor(private httpClient: HttpClient) {}
 
   getAll(url: string): Observable<PokemonApiResult> {
-    return this.httpClient.get<PokemonApiResult>(url);
+    return this.httpClient.get<PokemonApiResult>(url).pipe(delay(5000));
   }
 
   getByName(name: string): Observable<PokemonDetails> {
